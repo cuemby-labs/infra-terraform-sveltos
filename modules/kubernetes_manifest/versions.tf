2e2fbd5436060e421f2f0da048c4cacb588925e8
Manuel Nuila <manuel.nuila@cuemby.com>
2025-01-06 16:02:23 -0600

    update TF

--- modules/kubernetes_manifest/versions.tf
+++ modules/kubernetes_manifest/versions.tf
@@ -2,20 +2,12 @@ terraform {
   required_version = ">= 1.5.7"
 
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.11.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.23.0"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
       version = "~> 3.0"
     }
   }
--- versions.tf
+++ versions.tf
@@ -6,17 +6,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.11.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.2.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.23.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
     }
   }
 }
